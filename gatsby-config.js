942c3fd011e0b803846b1f44bd890f5cb556770f
ThemeMetric <thememetric@gmail.com>
2021-01-01 18:38:54 +0600

    Site Title changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 })
 module.exports = { 
   siteMetadata: {
-    title: `Infointeriors`,
+    title: `WPSERVICE`,
     description : `Hire a Freelance Developer for Programming Jobs`,
     author : `@ThemeMetric`,
     siteUrl: `https://wpservice.shweb.me`,
